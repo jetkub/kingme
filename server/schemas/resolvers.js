2a409ac9068034b64e76a35a59ba8cced6bf476d
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-02-28 00:08:17 -0500

    completed resolvers for now

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,52 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Game } = require('../models');
 const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('games')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('games');
+        }
+    },
+    Mutation: {
+        
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+
+    }
+}
+};
+
+module.exports = resolvers;
+
