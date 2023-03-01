0b0a3b87125dbdf64f1475905cb6f3ca595b2add
Jakub Cichon <jakub@uconn.edu>
2023-02-28 22:03:04 -0500

    add fonts

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -4,7 +4,12 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
+				// you can define some arbitrary names here and then use them in the className
+				// for example, you can use the className "font-base" to apply the "Space Grotesk" font
 				sans: ['Inter', 'sans-serif'],
+				serif: ['ui-serif', 'Georgia'],
+				base: ['"Space Grotesk"', 'sans-serif'],
+				hero: ['"Climate Crisis"', 'sans-serif'],
 			},
 		},
 	},
