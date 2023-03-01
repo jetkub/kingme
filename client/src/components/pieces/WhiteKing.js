4e75c935bca1fe83567d23ea41f26c18d1cb7fb4
theLemmonade <lemmonmail@icloud.com>
2023-02-28 19:14:51 -0500

    added react components

---
+++ client/src/components/pieces/WhiteKing.js
@@ -0,0 +1,10 @@
+
+import whiteKing from '../../assets/white-king.svg'
+
+function WhiteKing(props) {
+    return (
+        <img src={whiteKing} alt={'A white king.'} className={'piece'} />
+    )
+}
+
+export default WhiteKing
