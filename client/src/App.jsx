f7f3974fa4ca5edc61bb8d0ea9b7fcb2f6cb9e59
theLemmonade <lemmonmail@icloud.com>
2023-02-28 21:28:15 -0500

    further edits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import './App.css';
-import Row from './components/Row'
-import BlackPiece from './components/pieces/BlackPiece'
-import WhitePiece from './components/pieces/WhitePiece'
-import BlackKing from './components/pieces/BlackKing'
-import WhiteKing from './components/pieces/WhiteKing'
+import { useState } from "react";
+import "./App.css";
+import Menu from "./components/Menu";
+import Row from "./components/Row";
+import BlackPiece from "./components/pieces/BlackPiece";
+import WhitePiece from "./components/pieces/WhitePiece";
+import BlackKing from "./components/pieces/BlackKing";
+import WhiteKing from "./components/pieces/WhiteKing";
 
 function App() {
-  const data = [
-    [null, BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece],
-    [BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece, null],
-    [null, BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece, null],
-    [null, WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece],
-    [WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece, null]
-  ]
+	const data = [
+		[null, BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece],
+		[BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece, null],
+		[null, BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece],
+		[null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null],
+		[WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece, null],
+		[null, WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece],
+		[WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece, null],
+	];
 
-  return (
-    <table id="board">
-      <tbody>
-        {data.map((rowData, index) => {
-          const number = data.length - index
-          
-          return <Row key={number.toString()} number={number} data={rowData} />
-        })}
-      </tbody>
-    </table>
-  );
+	return (
+		<table id="board">
+			<tbody>
+				{data.map((rowData, index) => {
+					const number = data.length - index;
+
+					return <Row key={number.toString()} number={number} data={rowData} />;
+				})}
+			</tbody>
+		</table>
+	);
 }
 
 export default App;
