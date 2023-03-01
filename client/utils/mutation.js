3c4a51a231267e9feffa4f7c9b1c82a57ea1fe8f
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-02-28 20:24:49 -0500

    added new query to the resolver as well as added the mutation and queries to the utils folder in client side

---
+++ client/utils/mutation.js
@@ -0,0 +1,25 @@
+import {gql} from '@apollo/client';
+
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
