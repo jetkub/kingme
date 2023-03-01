fb7c877edde2375c6f408ed35e4769563e02a905
Ann <ann.hixson@uconn.edu>
2023-02-28 19:10:20 -0500

    added initial board layout

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,23 @@
 import { useState } from "react";
 
+function Square({ value, onSquareClick }) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
 export default function Board() {
     const [blackIsNext, setBlackIsNext] = useState(true);
 
-    const [squares, setSquares] = useState(Array(64).fill(null));
+    const [squares, setSquares] = useState(initialBoardLayout);
 
-    
+
+}
+
+// const pieceTypes = ['red', 'black', 'redKing', 'blackKing'];
+
+// const redPieceArray = []
+
+const initialBoardLayout = [null, 'black', null, 'black', null, 'black', null, 'black', 'black', null, 'black', null, 'black', null, 'black', null, null, 'black', null, 'black', null, 'black', null, 'black', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 'red', null, 'red', null, 'red', null, 'red', null, null, 'red', null, 'red', null, 'red', null, 'red', 'red', null, 'red', null, 'red', null, 'red', null]
+
+function movePiece() {
+
 }
