9f3c4d43c771143046f35812111942b4c877f411
Jakub Cichon <jakub@uconn.edu>
2023-02-28 20:52:15 -0500

    add router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/tailwind.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/tailwind.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</React.StrictMode>,
-	document.getElementById("root")
+	document.getElementById('root')
 );
