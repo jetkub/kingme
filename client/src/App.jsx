98a26e75834f0f90554e98fb1d1f2ea74f1f5ebe
Ann <ann.hixson@uconn.edu>
2023-03-02 19:37:18 -0500

    fixed merge conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,28 +7,18 @@ import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
-	const data = [
-		[null, BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece],
-		[BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece, null],
-		[null, BlackPiece, null, BlackPiece, null, BlackPiece, null, BlackPiece],
-		[null, null, null, null, null, null, null, null],
-		[null, null, null, null, null, null, null, null],
-		[WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece, null],
-		[null, WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece],
-		[WhitePiece, null, WhitePiece, null, WhitePiece, null, WhitePiece, null],
-	];
-
-	return (
-		<table id="board">
-			<tbody>
-				{data.map((rowData, index) => {
-					const number = data.length - index;
-
-					return <Row key={number.toString()} number={number} data={rowData} />;
-				})}
-			</tbody>
-		</table>
+		return (
+		<div className='bg-stone-200 min-h-screen'>
+			<Header />
+			<main className='flex flex-col items-center justify-center m-4'>
+				<Routes>
+					<Route path='/' element={<Menu />} />
+					<Route path='/game' element={<Checkers />} />
+				</Routes>
+			</main>
+		</div>
 	);
+
 }
 
 export default App;
