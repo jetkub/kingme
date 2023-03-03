340d18c0e022115a0f3fffd0ca02ec26d264ac4e
theLemmonade <lemmonmail@icloud.com>
2023-03-02 19:20:26 -0500

    bug fix

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -3,30 +3,26 @@ import React from 'react'
 const Menu = () => {
 	return (
 		<div id="menu">
-		<section id="hotseatContainer">
+		  <section id="hotseatContainer">
 			<div id="deco2">
 			  <img id="decoImg" src="./assets/crownwhite.png" style={{transform: 'rotate(10deg)'}} />
 			</div>
 			<div id="deco">
-				<img
-					id="decoImg"
-					src="./assets/crown.png"
-					style="transform: rotate(-10deg);"
-				/>
+			  <img id="decoImg" src="./assets/crown.png" style={{transform: 'rotate(-10deg)'}} />
 			</div>
 			<button id="hotseatBtn">Play Solo</button>
-		</section>
-		<section id="multiplayerContainer" style="display: flex">
-			<div id="hostContainer" style="flex: 1">
-				<button id="hostBtn">Host a Game</button>
+		  </section>
+		  <section id="multiplayerContainer" style={{display: 'flex'}}>
+			<div id="hostContainer" style={{flex: 1}}>
+			  <button id="hostBtn">Host a Game</button>
 			</div>
-			<div id="joinContainer" style="flex: 1">
-				<input id="joinInput" />
-				<button id="joinBtn">Join a Friend</button>
+			<div id="joinContainer" style={{flex: 1}}>
+			  <input id="joinInput" maxLength={5} />
+			  <button id="joinBtn">Join a Friend</button>
 			</div>
-		</section>
-	</div>
-	)
+		  </section>
+		</div>
+	  );
 }
 
 export default Menu
