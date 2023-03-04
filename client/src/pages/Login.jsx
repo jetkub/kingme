db0c9d43bc5d4796f142268b4b8c54a513381000
Jakub Cichon <jakub@uconn.edu>
2023-03-04 18:36:32 -0500

    add Login component and stlying to LoginPage

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import FormHeader from '../components/FormHeader';
+import Login from '../components/Login';
 
 const LoginPage = () => {
 	return (
 		<>
-			<FormHeader
-				heading='Login to your account'
-				paragraph="Don't have an account yet? "
-				linkName='Signup'
-				linkUrl='/signup'
-			/>
+			<div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+				<div className='w-full max-w-md space-y-8'>
+					<FormHeader
+						heading='Login to your account'
+						paragraph="Don't have an account yet? "
+						linkName='Signup'
+						linkUrl='/signup'
+					/>
+					<Login />
+				</div>
+			</div>
 		</>
 	);
 };
