f74d169c43a47311a495a963e128d8de40718f57
Jakub Cichon <jakub@uconn.edu>
2023-03-04 18:41:50 -0500

    create login component by mapping log fields over Input component. use state for loginState

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { React, useState } from 'react';
+import { loginFields } from '../constants/formFields';
+import Input from './Input';
+
+const fields = loginFields;
+let fieldsState = {};
+fields.forEach((field) => (fieldsState[field.id] = ''));
 
 const Login = () => {
-	return (
-		<div>Login</div>
-	)
-}
+	const [loginState, setLoginState] = useState(fieldsState);
 
-export default Login
+	const handleChange = (e) => {
+		setLoginState({ ...loginState, [e.target.id]: e.target.value });
+	};
+
+	return (
+		<form className='mt-8 space-y-6'>
+			<div className='-space-y-px'>
+				{fields.map((field) => (
+					<Input
+						key={field.id}
+						handleChange={handleChange}
+						value={loginState[field.id]}
+						labelText={field.labelText}
+						labelFor={field.labelFor}
+						id={field.id}
+						name={field.name}
+						type={field.type}
+						isRequired={field.isRequired}
+						placeholder={field.placeholder}
+					/>
+				))}
+			</div>
+		</form>
+	);
+};
+
+export default Login;
