4a470568cb5606b8ea888ebf9aea2735ac703173
Jakub Cichon <jakub@uconn.edu>
2023-03-04 18:11:48 -0500

    add array of form field values for login and signup forms

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import FormHeader from '../components/FormHeader';
 
 const LoginPage = () => {
 	return (
-		<div>Login Page</div>
-	)
-}
+		<>
+			<FormHeader
+				heading='Login to your account'
+				paragraph="Don't have an account yet? "
+				linkName='Signup'
+				linkUrl='/signup'
+			/>
+		</>
+	);
+};
 
-export default LoginPage
+export default LoginPage;
