28fb89abc41b0f2c92c645168ec551eb1319961c
Jakub Cichon <jakub@uconn.edu>
2023-03-04 18:01:46 -0500

    create formHeader component

--- client/src/components/FormHeader.jsx
+++ client/src/components/FormHeader.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import crown from '../assets/crown.png';
 
-const FormHeader = () => {
+const FormHeader = ({ heading, paragraph, linkName, linkUrl = '#' }) => {
 	return (
-		<div>FormHeader</div>
-	)
-}
+		<div className='mb-10'>
+			<div className='flex justify-center'>
+				<img alt='' className='h-14 w-14' src={crown} />
+			</div>
+			<h2 className='mt-6 text-center font-hero text-3xl text-gray-900'>{heading}</h2>
+			<p className='mt-5 text-center font-base text-base text-gray-600'>
+				{paragraph}{' '}
+				<Link to={linkUrl} className='font-medium text-neutral-900 hover:text-neutral-700'>
+					{linkName}
+				</Link>
+			</p>
+		</div>
+	);
+};
 
-export default FormHeader
+export default FormHeader;
