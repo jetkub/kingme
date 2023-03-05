b32f1debf0e0d0d450515fd452bf2936c17ecbc3
Jakub Cichon <jakub@uconn.edu>
2023-03-04 21:02:12 -0500

    add signup component and page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import FormHeader from '../components/FormHeader';
+import Signup from '../components/Signup';
 
 const SignupPage = () => {
 	return (
-		<div>SignupPage</div>
-	)
-}
+		<>
+			<div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+				<div className='w-full max-w-md space-y-8'>
+					<FormHeader
+						heading='Signup to create an account'
+						paragraph='Already have an account? '
+						linkName='Login'
+						linkUrl='/login'
+					/>
+					<Signup />
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default SignupPage
+export default SignupPage;
