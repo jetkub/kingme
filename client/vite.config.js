eb8444b6cbe55b3ceded08f7a4971beb297ccc04
Jakub Cichon <jakub@uconn.edu>
2023-03-05 17:58:50 -0500

    configure server proxy in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,38 +1,59 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {VitePWA} from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA({  
-      injectRegister: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],  
-      manifest: {  
-  name: 'websitename',  
-        short_name: 'websitename',  
-        description: 'Website description(Could be same with index.html file)',  
-        theme_color: '#ffffff',  
-        start_url: '/',  
-        icons: [  
-  {  
-  src: 'pwa-192x192.png',  
-            sizes: '192x192',  
-            type: 'image/png',  
-          },  
-          {  
-  src: 'pwa-512x512.png',  
-            sizes: '512x512',  
-            type: 'image/png',  
-          },  
-          {  
-  src: 'pwa-512x512.png',  
-            sizes: '512x512',  
-            type: 'image/png',  
-            purpose: 'any maskable',  
-          },  
-        ],  
-      },  
-    }),
-  ],
-})
+	server: {
+		proxy: {
+			'/graphql': {
+				target: 'http://localhost:3001',
+				changeOrigin: true,
+				secure: false,
+				configure: (proxy, _options) => {
+					proxy.on('error', (err, _req, _res) => {
+						console.log('proxy error', err);
+					});
+					proxy.on('proxyReq', (proxyReq, req, _res) => {
+						console.log('Sending Request to the Target:', req.method, req.url);
+					});
+					proxy.on('proxyRes', (proxyRes, req, _res) => {
+						console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+					});
+				},
+			},
+		},
+	},
+	plugins: [
+		react(),
+		VitePWA({
+			injectRegister: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+			manifest: {
+				name: 'websitename',
+				short_name: 'websitename',
+				description: 'Website description(Could be same with index.html file)',
+				theme_color: '#ffffff',
+				start_url: '/',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
+});
