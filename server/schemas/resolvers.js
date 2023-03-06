c0f82b34313ef8e1dccc1cb30ba51ba9bf0147ab
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-03-06 18:36:15 -0500

    redid resolvers once more

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,76 +1,82 @@
-const { AuthenticationError } = require("apollo-server-express");
-const { User, Game } = require("../models");
-const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Game } = require('../models');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
-  Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select("-__v -password")
-          .populate("games");
-        return userData;
-      }
-      throw new AuthenticationError("Not logged in");
-    },
-    users: async () => {
-      return User.find().select("-__v -password").populate("games");
-    },
-    user: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return User.findOne(params).select("-__v -password").populate("games");
-    },
-    games: async () => {
-      return Game.find().select("-__v").populate("players");
-    },
-    game: async (parent, { gameid }) => {
-        const params = gameid ? { gameId: gameid } : {};
-        return Game.findOne(params).select("-__v").populate("players");
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('games')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('games');
+        },
+        user: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return User.findOne(params).select('-__v -password').populate('games');
+    }
+},
+    Mutation: {
+        
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
         },
 
-  },
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-      return { token, user };
-    },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
 
-      if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
+            const correctPw = await user.isCorrectPassword(password);
 
-      const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
 
-      if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
+            const token = signToken(user);
+            return { token, user };
+},
 
-      const token = signToken(user);
-      return { token, user };
-    },
+        updateWins: async (parent, { _id }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $inc: { wins: 1, totalGames: 1 } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
 
-    updateWins: async (parent, { _id }) => {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: _id },
-        { $inc: { wins: 1, totalGames: 1 } },
-        { new: true }
-      );
-      return updatedUser;
-    },
+        updateLosses: async (parent, { _id }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $inc: { losses: 1, totalGames: 1 } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+}}
 
-    updateLosses: async (parent, { _id }) => {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: _id },
-        { $inc: { losses: 1, totalGames: 1 } },
-        { new: true }
-      );
-      return updatedUser;
-    },
-  },
-};
+
+
+
 
 module.exports = resolvers;
+
