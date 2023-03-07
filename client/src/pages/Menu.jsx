b3740e4d33e2e557f6aa8dab5236fa8859c6b71d
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-03-07 10:28:00 -0500

    added join player function not working just yet

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -4,6 +4,7 @@ import crown from "../assets/crown.png";
 
 import { useMutation, useQuery } from "@apollo/client";
 import { CREATE_GAME } from "../utils/mutation";
+import { ADD_PLAYER_TO_GAME } from "../utils/mutation";
 
 import { QUERY_ME } from "../utils/queries";
 
@@ -13,8 +14,12 @@ import { QUERY_ME } from "../utils/queries";
 const Menu = () => {
 
 	const [addGame] = useMutation(CREATE_GAME);
+	const [addPlayerToGame] = useMutation(ADD_PLAYER_TO_GAME);
 	const { loading, data } = useQuery(QUERY_ME);
 	const user = data?.me || {};
+
+	const input = document.getElementById("joinInput");
+	console.log(input.value);
 
 console.log(user);
 
@@ -31,6 +36,22 @@ console.log(user);
 			console.error(err);
 		}
 	};
+
+		const handleAddPlayer = async (e) => {
+			e.preventDefault();
+			try {
+				const { data } = await addPlayerToGame(
+					{
+						variables: { id: user._id,
+									gameid: input.value
+								},
+					}
+				);
+				console.log(data);
+			} catch (err) {
+				console.error(err);
+			}
+		};
 
 	return (
 		<>
@@ -66,8 +87,8 @@ console.log(user);
 						<button id="hostBtn" onClick={handleHost}>Host a Game</button>
 					</div>
 					<div id="joinContainer">
-						<input id="joinInput" maxLength={5} />
-						<button id="joinBtn">Join a Friend</button>
+						<input id="joinInput" maxLength={10} />
+						<button id="joinBtn" onClick={handleAddPlayer}>Join a Friend</button>
 					</div>
 				</section>
 			</div>
